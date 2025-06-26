59cb9d3a6af71a6c375e22d00b360dcb4041594a
irvis007 <irvis12@gmail.com>
2025-06-26 21:40:38 +0200

    chore(): Backup working set up

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -9,4 +7,30 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+
+  -- language suport
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.terraform" },
+
+  -- utilities
+  { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
+
+  { import = "astrocommunity.editing-support.hypersonic-nvim" },
+  { import = "astrocommunity.editing-support.multiple-cursors-nvim" },
+  { import = "astrocommunity.editing-support.nvim-devdocs" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  { import = "astrocommunity.editing-support.telescope-undo-nvim" },
+  { import = "astrocommunity.editing-support.text-case-nvim" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.editing-support.dial-nvim" },
+
+  { import = "astrocommunity.recipes.auto-session-restore" },
+
+  { import = "astrocommunity.scrolling.neoscroll-nvim" },
+
+  { import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
 }
