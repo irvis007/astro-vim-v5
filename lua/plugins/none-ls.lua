848994b9fac5a1c00bb1cf5577c9651f8e7c203b
irvis007 <irvis12@gmail.com>
2026-02-26 20:28:04 +0100

    fix(terraform): Remove redundant terraform configuration

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,22 +1,17 @@
--- Customize None-ls sources
+-- None-ls configuration (using default sources from language packs)
+-- To add custom formatters/linters, see:
+-- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins
 
 ---@type LazySpec
 return {
   "nvimtools/none-ls.nvim",
   opts = function(_, opts)
-    -- opts variable is the default configuration table for the setup function call
+    -- Using default sources from AstroNvim and language packs
+    -- Uncomment to add custom sources:
     -- local null_ls = require "null-ls"
-
-    -- Check supported formatters and linters
-    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-
-    -- Only insert new sources, do not replace the existing ones
-    -- (If you wish to replace, use `opts.sources = {}` instead of the `list_insert_unique` function)
-    opts.sources = require("astrocore").list_insert_unique(opts.sources, {
-      -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
-    })
+    -- opts.sources = require("astrocore").list_insert_unique(opts.sources, {
+    --   null_ls.builtins.formatting.stylua,
+    --   null_ls.builtins.formatting.prettier,
+    -- })
   end,
 }
